602584da25e74e6da1479b7155ee210ceaa5ee07
Unknown <hailey@athnex.com>
2018-10-19 00:56:57 -0700

    Added some handlers
    Added some handlers in smslisten and debugged some stuff. I am also starting some html to interface with it but I gotta put it on a server first.

--- smslisten.py
+++ smslisten.py
@@ -9,10 +9,9 @@ from flowroutenumbersandmessaging.flowroutenumbersandmessaging_client import Flo
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
+counter = 1
 basic_auth_user_name = os.environ.get('FR_ACCESS_KEY')
 basic_auth_password = os.environ.get('FR_SECRET_KEY')
-
 from_number = os.environ.get('FROM_NUMBER')
 
 client = FlowroutenumbersandmessagingClient(basic_auth_user_name, basic_auth_password)
@@ -20,30 +19,37 @@ messages_controller = client.messages
 
 #Flowroute API endpoint and reply SMS to be sent
 fr_api_url = "https://api.flowroute.com/v2.1/messages"
-reply_message = 'Thanks for the picture!'
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/inboundsms', methods=['POST'])
 def inboundsms():
-
-    #extract attributes from POSTed JSON of inbound MMS
+    global counter
+    #extract attributes from POSTed JSON of inbound SMS
     json_content = request.json
     reply_to = json_content['data']['attributes']['from']
     reply_from = json_content['data']['attributes']['to']
     body = json_content['data']['attributes']['body'].decode('utf-8')
 
     pprint.pprint(body)
-
-    #send a reply SMS from your Flowroute number
-    sendreply(reply_to, from_number, "You said " + body)
+    if body.lower() == u'count'.lower():
+        sendreply(reply_to, from_number, "There have been " + str(counter) + " messages sent to this system.")
+    elif body.lower() == u'help'.lower():
+        sendreply(reply_to, from_number, "Right now only the command 'count' works.")
+    else:       #Echo a reply
+        sendreply(reply_to, from_number, "What? You should type 'help' for a list of valid commands")
+    
     file = open("sms-logs.txt","a") 
- 
     file.write(reply_to +': ' + body + "\n") 
     file.close() 
-
+    counter += 1
     return '0'
+
+@app.route('/smscount', methods=['GET'])
+def smscount():
+    print("Returning the count of " + str(counter) + ".")
+    return str(counter)
 
 def sendreply(reply_to, reply_from, msg):
     request_body = '{ \
@@ -58,10 +64,9 @@ def sendreply(reply_to, reply_from, msg):
   } \
 }'
 
-    print ("---Send A Message")
+    print ("---Returning message")
     result = messages_controller.send_a_message(request_body)
-    pprint.pprint(result)
-    
+    #pprint.pprint(result)    
     return '0'
 
 if __name__ == '__main__':
