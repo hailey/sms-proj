9dea0519b30a4ddf4babed050265f23ea839ad1a
Hailey <hailey@athnex.com>
2019-10-08 15:09:16 -0700

    Had to unindent

--- app_settings.py
+++ app_settings.py
@@ -23,17 +23,17 @@ else:
 def appsettings():
     if not google_auth.is_logged_in():
         return flask.render_template('deny.html', denymsg = 'You are not authorized to be here', loggedin = False)
-        user_info = google_auth.get_user_info()
-        refreshtoken = google_auth.getRefreshToken()
-        if not refreshtoken:
-            return flask.render_template('error.html', denymsg = 'Error with your refresh token', loggedin = False)
-        userid = appdb.getUserIDfromGoogleID(user_info['id'])
-        if not userid:
-            return flask.render_template('error.html', denymsg = 'You are not currently logged in.', loggedin = False)
-        
-        rows = appdb.getDIDsbyAccount(userid)
-        return flask.render_template('settings.html',
-                                    name = user_info['name'],
-                                    picture = user_info['picture'],
-                                    dids = rows,
-                                    loggedin = True)
+    user_info = google_auth.get_user_info()
+    refreshtoken = google_auth.getRefreshToken()
+    if not refreshtoken:
+        return flask.render_template('error.html', denymsg = 'Error with your refresh token', loggedin = False)
+    userid = appdb.getUserIDfromGoogleID(user_info['id'])
+    if not userid:
+        return flask.render_template('error.html', denymsg = 'You are not currently logged in.', loggedin = False)
+
+    rows = appdb.getDIDsbyAccount(userid)
+    return flask.render_template('settings.html',
+                                name = user_info['name'],
+                                picture = user_info['picture'],
+                                dids = rows,
+                                loggedin = True)
